5296d99e65f48e5c698cbd3ad0dc12c5edb611d6
Aniruth <22245222+Aniruth-raman@users.noreply.github.com>
2024-06-02 15:20:56 +0530

    Added DRF,serializers and views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+)
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def sayHello(request):
- return HttpResponse('Hello World')
+    return HttpResponse("Hello World")
+
+
 def index(request):
-   return render(request, 'index.html', {})
+    return render(request, "index.html", {})
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
+    permission_classes=[IsAuthenticated]
